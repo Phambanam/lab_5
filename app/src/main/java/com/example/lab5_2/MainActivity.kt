8ad50156234197b5308ed6c38a3cd0abe1f02d5b
phambanam <phambanam1999.@gmail.com>
2021-11-26 13:37:30 +0300

    lab5_4 use picasso

--- app/src/main/java/com/example/lab5_2/MainActivity.kt
+++ app/src/main/java/com/example/lab5_2/MainActivity.kt
@@ -3,18 +3,20 @@ package com.example.lab5_2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.lab5_2.databinding.ActivityMainBinding
+import com.squareup.picasso.Picasso
 
 
 class MainActivity : AppCompatActivity() {
-    private val viewModel = LoadImageViewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        viewModel.downloadImage()
-        viewModel.bitmap.observe(this) { bitmap ->
-            binding.imageView.setImageBitmap(bitmap)
-        }
+         Picasso.get()
+             .load("https://hinhanhdephd.com/wp-content/uploads/2021/02/hinh-8-3-cho-me-2.jpg")
+             .placeholder(R.drawable.img)
+             .error(R.drawable.img_1)
+             .into(binding.imageView)
     }
 
 
