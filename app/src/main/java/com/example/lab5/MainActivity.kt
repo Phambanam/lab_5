eff7671d27b4136a87fa0670644568917ee121ad
phambanam <phambanam1999.@gmail.com>
2021-11-26 11:48:24 +0300

    lab5_1 use java thread

--- app/src/main/java/com/example/lab5/MainActivity.kt
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.lab5
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.lab5.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private  var secondsElapsed: Int = 0
+    private lateinit var sharedPreferences: SharedPreferences
+    private var backgroundThread = Thread {
+        try {
+            while (!Thread.currentThread().isInterrupted) {
+                Log.d("MainActivity", "${Thread.currentThread()} is iterating")
+                binding.textSecondsElapsed.post {
+                    binding.textSecondsElapsed.text = getString(R.string.sec_elapsed, secondsElapsed++)
+                }
+                Thread.sleep(1000)
+            }
+        } catch (e: InterruptedException) {
+            Thread.currentThread().interrupt()
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        sharedPreferences = getSharedPreferences("SECONDS", Context.MODE_PRIVATE)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        backgroundThread.start()
+        secondsElapsed = sharedPreferences.getInt("SECONDS", 0)
+        Log.d("TEST","thread start\nSECONDS = $secondsElapsed ")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        backgroundThread.interrupt()
+        sharedPreferences.edit().putInt("SECONDS", secondsElapsed).apply()
+        Log.d("TEST","Thread interrupted\n" +
+                "SECONDS = $secondsElapsed ")
     }
 }
