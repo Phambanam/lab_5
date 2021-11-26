3f0673b1d72ba214584b3d87115eda3d4d28484e
phambanam <phambanam1999.@gmail.com>
2021-11-26 13:26:13 +0300

    lab5_3

--- app/src/main/java/com/example/lab5_2/LoadImageViewModel.kt
+++ app/src/main/java/com/example/lab5_2/LoadImageViewModel.kt
@@ -5,26 +5,23 @@ import android.graphics.BitmapFactory
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.net.URL
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
 
-class LoadImageViewModel: ViewModel() {
+class LoadImageViewModel : ViewModel() {
     private val _bitmap = MutableLiveData<Bitmap>()
-    val bitmap : LiveData<Bitmap> = _bitmap
-    private lateinit var executorService: ExecutorService
+    val bitmap: LiveData<Bitmap> = _bitmap
 
-    fun downloadImage(){
-        executorService = Executors.newFixedThreadPool(1)
-        executorService.execute {
+    fun downloadImage() {
+        viewModelScope.launch(Dispatchers.IO) {
             val url = URL("https://hinhanhdephd.com/wp-content/uploads/2021/02/hinh-8-3-cho-me-2.jpg")
             val image = BitmapFactory.decodeStream(url.openConnection().getInputStream())
             _bitmap.postValue(image)
         }
+
     }
 
-    override fun onCleared() {
-        super.onCleared()
-        executorService.shutdown()
-    }
+
 }
