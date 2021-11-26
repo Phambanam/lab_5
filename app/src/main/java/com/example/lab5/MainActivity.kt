15a84c4d70e9648bb87f1fe3b5f9d3a7abd027f3
phambanam <phambanam1999.@gmail.com>
2021-11-26 12:08:53 +0300

    lab5_1 use coroutines

--- app/src/main/java/com/example/lab5/MainActivity.kt
+++ app/src/main/java/com/example/lab5/MainActivity.kt
@@ -5,48 +5,40 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.lab5.databinding.ActivityMainBinding
-import java.util.concurrent.Executor
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
+import kotlinx.coroutines.delay
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private  var secondsElapsed: Int = 0
     private lateinit var sharedPreferences: SharedPreferences
-    private lateinit var executorService: ExecutorService
-
-    private fun startCountTime(){
-        executorService = Executors.newFixedThreadPool(1)
-        executorService.execute{
-            while (!executorService.isShutdown) {
-                Log.d("TEST", "${Thread.currentThread()} is iterating")
-                binding.textSecondsElapsed.post {
-                    binding.textSecondsElapsed.text = getString(R.string.sec_elapsed, secondsElapsed++)
-                }
-                Thread.sleep(1000)
-            }
-        }
-    }
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         sharedPreferences = getSharedPreferences("SECONDS", Context.MODE_PRIVATE)
+
+        lifecycleScope.launchWhenStarted {
+            Log.d("TEST", "Coroutine is launching")
+            while(true){
+                Log.d("TEST", "coroutine is working ")
+                binding.textSecondsElapsed.post {
+                    binding.textSecondsElapsed.text = getString(R.string.sec_elapsed, secondsElapsed++)
+                }
+                delay(1000)
+            }
+        }
     }
 
     override fun onStart() {
         super.onStart()
-        startCountTime()
         secondsElapsed = sharedPreferences.getInt("SECONDS", 0)
         Log.d("TEST","thread start\nSECONDS = $secondsElapsed ")
     }
 
     override fun onStop() {
         super.onStop()
-       executorService.shutdown()
         sharedPreferences.edit().putInt("SECONDS", secondsElapsed).apply()
         Log.d("TEST","Thread interrupted\n" +
                 "SECONDS = $secondsElapsed ")
