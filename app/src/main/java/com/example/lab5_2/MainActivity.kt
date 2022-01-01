1b3c2e4da5d6aac6de40330814a8b2a59ae2fb43
phambanam <phambanam1999@gmail.com>
2022-01-01 09:38:23 +0300

    fix

--- app/src/main/java/com/example/lab5_2/MainActivity.kt
+++ app/src/main/java/com/example/lab5_2/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.lab5_2
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import com.example.lab5_2.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.URL
 
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +18,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        viewModel.downloadImage()
+        lifecycleScope.launch(Dispatchers.IO) {
+            Log.d("Test",Thread.currentThread().name)
+            val url = URL("https://hinhanhdephd.com/wp-content/uploads/2021/02/hinh-8-3-cho-me-2.jpg")
+            val image = BitmapFactory.decodeStream(url.openConnection().getInputStream())
+            withContext(Dispatchers.Main){
+                Log.d("Test",Thread.currentThread().name)
+                binding.imageView.setImageBitmap(image)}
+
+        }
+        /*viewModel.downloadImage()
         viewModel.bitmap.observe(this) { bitmap ->
             binding.imageView.setImageBitmap(bitmap)
-        }
+        }*/
     }
 
 
